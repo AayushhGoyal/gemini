dc1df586c14b5861d2a64d730d67beccd8f99c1a
AayushhGoyal <agayush088@gmail.com>
2024-06-15 18:15:59 +0530

    initial commit

---
+++ clone/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Sidebar from './components/Sidebar/Sidebar'
+import assets from './assets/assets'
+
+function App() {
+  return (
+    <div>
+
+      <div className="bg-black text-white">joker</div>
+
+      <Sidebar />
+    </div>
+  )
+}
+
+export default App
